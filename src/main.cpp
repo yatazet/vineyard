b2b99ad76e243eacab52cd24b9af89fc6925af0c
yatazet <max1011283628@gmail.com>
2023-04-27 18:24:10 +0300

    funfunfun

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,19 @@
 /*This is a main file of vineyard ||| zxweb*/
 #include <iostream>
 #include <algorithm>
+#include <cstdio>
 using namespace std;
 int main(int argc, char* argv[])
 {
+    char p; //parameter
+    p = getchar();
+    if (p == 'a')
+    {
+        cout << "Hello." << endl;
+    }
+    else 
+    {
+        cout << "Bye." << endl;
+    }
     return 0;
 }
